cdd3e209bc8505c357dbca51607af78e4b357fa7
joanterm <jo_joanarg@yahoo.com>
2021-04-19 13:41:29 -0400

    Added gsap animations to the entire site

--- src/gsapAnimation.js
+++ src/gsapAnimation.js
@@ -3,23 +3,45 @@ let tl = gsap.timeline()
 tl.from("#main", {
     duration: 2, 
     opacity: 0, 
-    // scale: 0.3, 
-    // ease:"back"
-    y: 600
+    //scale: 0.3, 
+    //ease:"elastic"
+    //x: 600
 })
     .from("#main-img", {
     x:500,
     opacity: 0,
     duration: 2
 }, "-=1")
+
+    .from("#mission", {
+    duration: 2, 
+    opacity: 0, 
+    }, "-=3")
+
+    .from("#mission-info", {
+    duration: 2, 
+    opacity: 0,
+    }, "-=3")
     
 
-gsap.from("#trends", {
+gsap.from(["#trends", "#trends-info"], {
     scrollTrigger: {
         trigger: "#trends",
-        start: "bottom center"
+        start: "top center"
     },
-    duration: 4, 
+    duration: 2, 
+    opacity: 0, 
+    // scale: 0.3, 
+    // ease:"power1"
+
+})
+
+gsap.from(["#collection"], {
+    scrollTrigger: {
+        trigger: "#collection",
+        start: "top center"
+    },
+    duration: 2, 
     opacity: 0, 
     // scale: 0.3, 
     // ease:"power1"
